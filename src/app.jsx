a6d55997552b88fd97e7fa8e7936f2b7e5c7b80f
bbusraozturkk <202003011037@dogus.edu.tr>
2025-06-01 16:50:19 +0300

    App.jsx dosyasına console.log eklendi

--- src/app.jsx
+++ src/app.jsx
@@ -41,7 +41,8 @@ export default function App() {
   console.info(`%c${charAt}`, 'color: #5BE49B');
 
   useScrollToTop();
-
+  console.log("test");
+  
   return (
     <AuthProvider>
       <LocalizationProvider>
